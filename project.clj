789284a2d4109e62ba424c7ba9190c45d30c51f6
Sam Ritchie <sritchie09@gmail.com>
2011-11-08 21:57:18 -0800

    Added variance predicate macro.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Useful extensions to Cascalog."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [cascalog "1.8.3"]]
-  :dev-dependencies [[swank-clojure "0.4.0-SNAPSHOT"]
+  :dev-dependencies [[swank-clojure "1.4.0-SNAPSHOT"]
                      [org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [lein-multi "1.1.0-SNAPSHOT"]]
   :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
--- src/cascalog/contrib/stats.clj
+++ src/cascalog/contrib/stats.clj
@@ -1,1 +1,15 @@
-(ns cascalog.contrib.stats)
+(ns cascalog.contrib.stats
+  (:use cascalog.api)
+  (:require [cascalog.ops :as c]))
+
+(def variance
+  "Predicate macro that calculates the variance of the supplied input
+   var."
+  (<- [!val :> !var]
+      (* !val !val :> !squared)
+      (c/sum !squared :> !square-sum)
+      (c/count !count)
+      (c/avg !val :> !mean)
+      (* !mean !mean :> !mean-squared)
+      (div !square-sum !count :> !i)
+      (- !i !mean-squared :> !var)))
