51da8be2a9ee2854fd1df2567b6b0042a1625d92
Sam Ritchie <sritchie09@gmail.com>
2011-12-16 15:03:19 -0800

    Bump project.clj heap space opts.

--- lein-cascalog/src/leiningen/new/cascalog/project.clj
+++ lein-cascalog/src/leiningen/new/cascalog/project.clj
@@ -1,6 +1,7 @@
 (defproject {{name}} "0.1.0-SNAPSHOT"
   :description "TODO: Project Description."
   :source-path "src/clj"
+  :jvm-opts ["-Xmx768m" "-server"]
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [cascalog "1.8.4"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
