35ce7c2ba5586da6e15818e24ba4001d34bd2a80
Sam Ritchie <sritchie09@gmail.com>
2012-01-03 14:19:19 -0800

    upgrade to edb 2.0 interfaces.

--- cascalog.elephantdb/src/clj/elephantdb/cascalog/conf.clj
+++ cascalog.elephantdb/src/clj/elephantdb/cascalog/conf.clj
@@ -1,1 +1,30 @@
-(ns elephantdb.cascalog.conf)
+(ns elephantdb.cascalog.conf
+  (:import [elephantdb.cascading ElephantBaseTap$Args]
+           [java.util ArrayList HashMap]))
+
+(defn mk-hash-map [m]
+  (HashMap. (or m {})))
+
+(defn- mk-list [l]
+  (when l (ArrayList. l)))
+
+(def default-args
+  {:persistence-options {}
+   :indexer      nil
+   :version      nil
+   :tmp-dirs     nil
+   :timeout-ms   nil})
+
+(defn convert-clj-args
+  [{:keys [persistence-options tmp-dirs indexer timeout-ms version]}]
+  (let [ret (ElephantBaseTap$Args.)]
+    (set! (.persistenceOptions ret) (mk-hash-map persistence-options))
+    (set! (.tmpDirs ret)            (mk-list tmp-dirs))
+    (when indexer
+      (set! (.indexer ret) indexer))
+    (when timeout-ms
+      (set! (.timeoutMs ret) timeout-ms))
+    (set! (.version ret) version)
+    ret))
+
+
--- cascalog.elephantdb/src/clj/elephantdb/cascalog/impl.clj
+++ cascalog.elephantdb/src/clj/elephantdb/cascalog/impl.clj
@@ -1,55 +1,32 @@
 (ns elephantdb.cascalog.impl
   (:use cascalog.api)
   (:import [cascalog.ops IdentityBuffer]
-           [elephantdb.hadoop Common]
            [elephantdb Utils]
-           [elephantdb.persistence Coordinator]
-           [elephantdb.cascading ElephantDBTap ElephantDBTap$Args]
-           [java.util ArrayList HashMap]
+           [elephantdb.partition ShardingScheme]
+           [elephantdb.serialize Serializer]
            [org.apache.hadoop.io BytesWritable]))
 
-(defn- serializable-persistence-options
-  [options]
-  (HashMap. options))
-
-(defn- serializable-list [l]
-  (when l (ArrayList. l)))
-
-(defn convert-clj-args
-  [a {:keys [persistence-options tmp-dirs indexer timeout-ms version]}]
-  (let [ret (ElephantDBTap$Args.)]
-    (set! (.persistenceOptions ret) (HashMap. persistence-options))
-    (set! (.tmpDirs ret)            (serializable-list tmp-dirs))
-    (set! (.indexer ret) indexer)
-    (when timeout-ms
-      (set! (.timeoutMs ret) timeout-ms))
-    (set! (.version ret) version)
-    ret))
-
-(defmapop [shardify [^Integer num-shards]] [k]
-  (Utils/keyShard (Common/serializeElephantVal k)
-                  num-shards))
+(defmapop [shard [^ShardingScheme scheme shard-count]]
+  "Returns the shard to which the supplied shard-key should be
+  routed."
+  [shard-key]
+  (.shardIndex scheme shard-key shard-count))
 
 ;; TODO: Replace this serialization method with the kryo ObjectBuffer.
-(defmapop [mk-sortable-key [^Coordinator coordinator]]
-  "TODO: We can't just use Common/serializeElephantVal; we need to use
-  a serializer from the LocalPersistenceFactory."
-  [k]
+(defmapop [mk-sortable-key [^Serializer serializer]]
+  [shard-key]
   (BytesWritable.
-   (.getSortableKey
-    (.getKeySorter coordinator)
-    (Common/serializeElephantVal k))))
+   (.serialize serializer shard-key)))
 
 (defn elephant<-
-  "Keys are serialized for you. You have to go ahead and serialize
-  values. TODO: Change this."
   [elephant-tap kv-src]
   (let [spec        (.getSpec elephant-tap)
-        lp-factory  (.getLPFactory spec)
-        shard-count (.getNumShards spec)]
+        scheme      (.getShardScheme spec)
+        shard-count (.getNumShards spec)
+        serializer  (Utils/makeSerializer spec)]
     (<- [!shard !key !value]
         (kv-src !keyraw !valueraw)
-        (mk-sortable-key [lp-factory] !keyraw :> !sort-key)
-        (shardify [shard-count] !keyraw :> !shard)
+        (shard [scheme shard-count] !keyraw :> !shard)
+        (mk-sortable-key [serializer] !keyraw :> !sort-key)
         (:sort !sort-key)
         ((IdentityBuffer.) !keyraw !valueraw :> !key !value))))
