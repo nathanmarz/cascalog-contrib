f9633b03987ef30239e2a595575b70fc38845597
Sam Ritchie <sritchie09@gmail.com>
2012-01-09 12:16:05 -0500

    version bump.

--- cascalog.elephantdb/project.clj
+++ cascalog.elephantdb/project.clj
@@ -8,8 +8,8 @@
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [midje-cascalog "0.3.0"]]
   :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
-                      [cascalog "1.8.5-SNAPSHOT"]
+                      [cascalog "1.8.5"]
                       [elephantdb/elephantdb-cascading "0.2.0"]]
                "1.4" [[org.clojure/clojure "1.4.0-alpha2"]
-                      [cascalog "1.8.5-SNAPSHOT"]
+                      [cascalog "1.8.5"]
                       [elephantdb/elephantdb-cascading "0.2.0"]]})
