d6ed15d4fa06b0fbaa35694ac2d3b3c1098ee7f0
Sam Ritchie <sritchie09@gmail.com>
2012-01-09 13:25:27 -0500

    bump to new edb interface.

--- cascalog.elephantdb/project.clj
+++ cascalog.elephantdb/project.clj
@@ -1,15 +1,15 @@
-(defproject cascalog-elephantdb "0.2.0"
+(defproject cascalog-elephantdb "0.3.0"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [cascalog "1.8.5"]
-                 [elephantdb/elephantdb-cascading "0.2.0"]]
+                 [cascalog "1.9.0-wip"]
+                 [elephantdb/elephantdb-cascading "0.3.0"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [midje-cascalog "0.3.0"]]
   :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
-                      [cascalog "1.8.5"]
-                      [elephantdb/elephantdb-cascading "0.2.0"]]
-               "1.4" [[org.clojure/clojure "1.4.0-alpha2"]
-                      [cascalog "1.8.5"]
-                      [elephantdb/elephantdb-cascading "0.2.0"]]})
+                      [cascalog "1.9.0-wip"]
+                      [elephantdb/elephantdb-cascading "0.3.0"]]
+               "1.4" [[org.clojure/clojure "1.4.0-alpha3"]
+                      [cascalog "1.9.0-wip"]
+                      [elephantdb/elephantdb-cascading "0.3.0"]]})
