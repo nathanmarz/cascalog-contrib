f509d69c0b31d8acff45610524cb4e7540945211
Sam Ritchie <sritchie09@gmail.com>
2012-01-11 00:40:26 -0500

    Added recompute option to tap.

--- cascalog.elephantdb/src/clj/cascalog/elephantdb/conf.clj
+++ cascalog.elephantdb/src/clj/cascalog/elephantdb/conf.clj
@@ -11,20 +11,22 @@
 (def default-args
   {:persistence-options {}
    :indexer      nil
+   :recompute    false
    :version      nil
    :tmp-dirs     nil
    :timeout-ms   nil})
 
 (defn convert-clj-args
-  [{:keys [persistence-options tmp-dirs indexer timeout-ms version]}]
+  [{:keys [persistence-options tmp-dirs] :as opts}]
   (let [ret (ElephantBaseTap$Args.)]
     (set! (.persistenceOptions ret) (mk-hash-map persistence-options))
     (set! (.tmpDirs ret)            (mk-list tmp-dirs))
-    (when indexer
+    (set! (.recompute ret) (:recompute opts))
+    (when-let [indexer (:indexer opts)]
       (set! (.indexer ret) indexer))
-    (when timeout-ms
+    (when-let [timeout-ms (:timeout-ms opts)]
       (set! (.timeoutMs ret) timeout-ms))
-    (set! (.version ret) version)
+    (set! (.version ret) (:version opts))
     ret))
 
 
