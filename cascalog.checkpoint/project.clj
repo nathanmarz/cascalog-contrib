d518a9bef0b90f0521175ae611311e2e3e527d8d
Sam Ritchie <sritchie09@gmail.com>
2012-01-12 14:58:34 -0500

    Share dependencies.

--- cascalog.checkpoint/project.clj
+++ cascalog.checkpoint/project.clj
@@ -1,19 +1,15 @@
+(def shared
+  '[[cascalog "1.8.5"]
+    [jackknife "0.1.2"]
+    [hadoop-util "0.2.7"]])
+
 (defproject cascalog-checkpoint "0.1.1"
   :description "Workflow checkpoints for the masses."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                 [cascalog "1.8.5"]
-                 [jackknife "0.1.2"]
-                 [hadoop-util "0.2.7"]]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [lein-multi "1.1.0-SNAPSHOT"]
                      [midje-cascalog "0.3.1"]]
-  :multi-deps {"1.2" [[org.clojure/clojure "1.2.1"]
-                      [cascalog "1.8.5"]
-                      [jackknife "0.1.2"]
-                      [hadoop-util "0.2.7"]]
-               "1.4" [[org.clojure/clojure "1.4.0-alpha3"]
-                      [cascalog "1.8.5"]
-                      [jackknife "0.1.2"]
-                      [hadoop-util "0.2.7"]]})
+  :dependencies ~(conj shared '[org.clojure/clojure "1.3.0"])
+  :multi-deps {"1.2" ~(conj shared '[org.clojure/clojure "1.2.1"])
+               "1.4" ~(conj shared '[org.clojure/clojure "1.4.0-alpha3"])})
 
 
--- cascalog.elephantdb/project.clj
+++ cascalog.elephantdb/project.clj
@@ -7,8 +7,8 @@
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
-  :dependencies ~(conj shared '[org.clojure/clojure "1.3.0"])
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [midje-cascalog "0.3.1"]]
+  :dependencies ~(conj shared '[org.clojure/clojure "1.3.0"])
   :multi-deps {"1.2" ~(conj shared '[org.clojure/clojure "1.2.1"])
                "1.4" ~(conj shared '[org.clojure/clojure "1.4.0-alpha3"])})
