c4074d9bbd5d77f4eb1baaccfa98d5281dbf3c96
Sam Ritchie <sritchie09@gmail.com>
2012-02-25 16:03:51 -0800

    name change and version bump.

--- cascalog.elephantdb/src/clj/cascalog/elephantdb/impl.clj
+++
@@ -1,7 +0,0 @@
-(ns cascalog.elephantdb.impl
-  (:use cascalog.api)
-  (:import [cascalog.ops IdentityBuffer]
-           [elephantdb Utils]
-           [elephantdb.partition ShardingScheme]
-           [elephantdb.serialize Serializer]
-           [org.apache.hadoop.io BytesWritable]))
--- cascalog.lzo/project.clj
+++ cascalog.lzo/project.clj
@@ -1,7 +1,7 @@
-(defproject cascalog-lzo "0.1.0-wip6"
+(defproject cascalog-lzo "0.1.0-wip7"
   :description "Lzo compression taps for Cascalog."
   :repositories {"bird" "https://raw.github.com/kevinweil/elephant-bird/master/repo"}
-  :dependencies [[cascalog "1.9.0-wip6"]
+  :dependencies [[cascalog "1.9.0-wip7"]
                  [cascalog/elephant-bird "2.1.9"]
                  [hadoop-lzo "0.4.14"]]
   :exclusions [yamlbeans
