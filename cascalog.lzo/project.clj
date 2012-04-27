a47988826dfc574fa943a6395802144f609d08a4
Sam Ritchie <sritchie09@gmail.com>
2012-04-27 13:55:19 -0700

    fixed elephant-bird versions.

--- cascalog.lzo/project.clj
+++ cascalog.lzo/project.clj
@@ -1,16 +1,19 @@
-(defproject cascalog-lzo "0.1.0-wip10"
+(defproject cascalog-lzo "0.1.0-wip11"
   :description "Lzo compression taps for Cascalog."
   :repositories {"bird" "https://raw.github.com/kevinweil/elephant-bird/master/repo"}
   :dependencies [[cascalog "1.9.0-wip10"]
-                 [cascalog/elephant-bird "2.1.9"]
-                 [hadoop-lzo "0.4.14"]]
+                 [com.twitter/elephant-bird "2.2.2"]
+                 [hadoop-lzo "0.4.15"]]
   :exclusions [yamlbeans
                com.google/protobuf-java
                com.hadoop/hadoop-lzo
                org.apache.thrift/libthrift
                org.apache.pig/pig
+               org.apache.pig/piggybank
                org.apache.hive/hive-serde
+               org.apache.hive/hive-exec
                org.apache.hadoop/hadoop-core
-               org.apache.mahout/mahout-core]
+               org.apache.mahout/mahout-core
+               org.apache.hcatalog/hcatalog]
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [midje-cascalog "0.4.0"]])
