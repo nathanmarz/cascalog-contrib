c4b120e9bb0f0b53bc0417e3582624e9cc2ff86f
Sam Ritchie <sritchie09@gmail.com>
2012-05-02 13:35:06 -0700

    bump cascalog.elephantdb to 0.3.2.

--- cascalog.elephantdb/project.clj
+++ cascalog.elephantdb/project.clj
@@ -1,14 +1,14 @@
 (def shared
-  '[[cascalog "1.9.0-wip7"]
-    [elephantdb/elephantdb-cascading "0.3.1"]])
+  '[[cascalog "1.9.0-wip10"]
+    [elephantdb/elephantdb-cascading "0.3.2"]])
 
-(defproject cascalog-elephantdb "0.3.1"
+(defproject cascalog-elephantdb "0.3.2"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [midje-cascalog "0.4.0"]
-                     [hadoop-util "0.2.7"]]
+                     [hadoop-util "0.2.8"]]
   :dependencies ~(conj shared '[org.clojure/clojure "1.3.0"])
   :multi-deps {"1.2" ~(conj shared '[org.clojure/clojure "1.2.1"])
                "1.4" ~(conj shared '[org.clojure/clojure "1.4.0-alpha3"])})
