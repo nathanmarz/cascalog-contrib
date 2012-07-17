ca9bb5b6935ed75b6a2bc38d22efd77fc87e2219
Paul Lam <paul.lam@forward.co.uk>
2012-07-17 16:10:09 +0100

    move all deps into :dev profile

--- cascalog.more-taps/project.clj
+++ cascalog.more-taps/project.clj
@@ -1,10 +1,15 @@
 (defproject cascalog-more-taps "0.3.0-SNAPSHOT"
   :description "Taps for Cascalog"
+  :url "https://github.com/nathanmarz/cascalog-contrib"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cascalog "1.10.0"]
-                 [cascading/cascading-hadoop "2.0.2-wip-324"]
-                 [org.pingles/cascading.protobuf "0.0.1"]]
-  :profiles {:all {:dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]]}}
+  :dependencies [[org.pingles/cascading.protobuf "0.0.1"]] 
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.4.0"]
+                                  [cascalog "1.10.0"]
+                                  [cascading/cascading-hadoop "2.0.2-wip-324"]
+                                  [org.apache.hadoop/hadoop-core "0.20.2-dev"]]}}
   :repositories {"conjars.org" "http://conjars.org/repo"}
-  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]])
+  :dev-dependencies [[org.clojure/clojure "1.4.0"]
+                     [cascalog "1.10.0"]
+                     [cascading/cascading-hadoop "2.0.2-wip-324"]
+                     [org.pingles/cascading.protobuf "0.0.1"]
+                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]])
