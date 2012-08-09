b9b0aaf0716b8040a8ed4b94c0ccfa962a71e551
Soren Macbeth <soren@dopeness.org>
2012-08-09 10:01:35 -0700

    bump deps

--- cascalog.elephantdb/project.clj
+++ cascalog.elephantdb/project.clj
@@ -1,8 +1,8 @@
 (def shared
-  '[[cascalog "1.9.0-wip10"]
-    [yieldbot/elephantdb-cascading "0.3.2"]])
+  '[[cascalog "1.10.0"]
+    [yieldbot/elephantdb-cascading "0.3.4"]])
 
-(defproject yieldbot/cascalog-elephantdb "0.3.2"
+(defproject yieldbot/cascalog-elephantdb "0.3.4"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
@@ -11,4 +11,4 @@
                      [hadoop-util "0.2.8"]]
   :dependencies ~(conj shared '[org.clojure/clojure "1.3.0"])
   :multi-deps {"1.2" ~(conj shared '[org.clojure/clojure "1.2.1"])
-               "1.4" ~(conj shared '[org.clojure/clojure "1.4.0-alpha3"])})
+               "1.4" ~(conj shared '[org.clojure/clojure "1.4.0"])})
