c8f661b6434feb8fa4645afcef7b35a66b73478f
Soren Macbeth <soren@dopeness.org>
2012-08-15 10:29:43 -0700

    bump deps and move to 0.3.5-SNAPSHOT

--- cascalog.elephantdb/project.clj
+++ cascalog.elephantdb/project.clj
@@ -1,13 +1,14 @@
 (def shared
-  '[[cascalog "1.10.0"]
-    [yieldbot/elephantdb-cascading "0.3.4"]])
+  '[[cascalog "1.10.1-SNAPSHOT"]
+    [yieldbot/elephantdb-cascading "0.3.5-SNAPSHOT"]])
 
-(defproject yieldbot/cascalog-elephantdb "0.3.4"
+(defproject yieldbot/cascalog-elephantdb "0.3.5-SNAPSHOT"
   :source-path "src/clj"
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
   :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [midje-cascalog "0.4.0"]
+                     [midje-cascalog "0.4.0"
+                      :exclusions [org.clojure/clojure]]
                      [hadoop-util "0.2.8"]]
   :dependencies ~(conj shared '[org.clojure/clojure "1.3.0"])
   :multi-deps {"1.2" ~(conj shared '[org.clojure/clojure "1.2.1"])
