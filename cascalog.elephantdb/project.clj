d41448424d2fc753e892afd1ce2595964ff88454
Soren Macbeth <soren@dopeness.org>
2013-01-05 11:07:56 -0800

    update project to lein2

--- cascalog.elephantdb/project.clj
+++ cascalog.elephantdb/project.clj
@@ -3,13 +3,21 @@
     [yieldbot/elephantdb-cascading "0.3.5-SNAPSHOT"]])
 
 (defproject yieldbot/cascalog-elephantdb "0.3.5-SNAPSHOT"
-  :source-path "src/clj"
-  :java-source-path "src/jvm"
-  :javac-options {:debug "true" :fork "true"}
-  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [midje-cascalog "0.4.0"
-                      :exclusions [org.clojure/clojure]]
-                     [hadoop-util "0.2.8"]]
+  :min-lein-version "2.0.0"
+  :source-paths ["src/clj"]
+  :java-source-paths ["src/jvm"]
+  :javac-options ["-source" "1.6" "-target" "1.6"]
+  :jvm-opts ["-server" "-Xmx768m"]
   :dependencies ~(conj shared '[org.clojure/clojure "1.4.0"])
-  :multi-deps {"1.2" ~(conj shared '[org.clojure/clojure "1.2.1"])
-               "1.3" ~(conj shared '[org.clojure/clojure "1.3.0"])})
+  :profiles {:1.2 {:dependencies [[org.clojure/clojure "1.2.1"]]}
+             :1.3 {:dependencies [[org.clojure/clojure "1.3.0"]]}
+             :1.5 {:dependencies [[org.clojure/clojure "1.5.0-RC1"]]}
+             :dev
+             {:dependencies
+              [[org.apache.hadoop/hadoop-core "1.0.3"]
+               [midje-cascalog "0.4.0"
+                :exclusions [org.clojure/clojure]]
+               [hadoop-util "0.2.8"]]
+              :plugins [[lein-midje "2.0.3"
+                         lein-pedantic "0.0.5"]]}}
+  :pedantic :warn)
